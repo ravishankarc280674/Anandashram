6e7bde77a8f77ed15961f88d7213bcc4e7de680a
ravishankarc280674 <ravishankarc@yahoo.co.in>
2025-12-07 04:48:34 +0530

    Horizontal bar chart done

---
+++ Anandashram.UI/DTO/TimelineDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Anandashram.DTO;
+
+public class TimelineDTO
+{
+    public string RoomName { get; set; }
+    public string DevoteeName { get; set; }
+    public string DevoteeCode { get; set; }
+    public int Allocated { get; set; }
+    public DateTime FromDate { get; set; }
+    public DateTime ToDate { get; set; }
+}
