a1f57a80a99914fb9348d82d43a6c7913d579195
ravishankarc280674 <ravishankarc@yahoo.co.in>
2025-12-07 18:07:10 +0530

    changed header style

--- Anandashram.UI/Controllers/ReservationController.cs
+++ Anandashram.UI/Controllers/ReservationController.cs
@@ -1,4 +1,7 @@
-﻿namespace Anandashram.Controllers;
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace Anandashram.Controllers;
 [Authorize]
 public class ReservationController : Controller
 {
@@ -58,6 +61,60 @@ public class ReservationController : Controller
         var data = await _reservationService.GetReservationsForChart(startDate, endDate);
         return Json(data);
     }
+   
+    public async Task<IActionResult> ExportTimelineExcel(DateTime startDate, DateTime endDate)
+    {
+        var list = await _reservationService.GetReservationsForChart(startDate, endDate);
+
+        using var stream = new MemoryStream();
+        using (var spreadsheet = SpreadsheetDocument.Create(stream, SpreadsheetDocumentType.Workbook))
+        {
+            var workbookPart = spreadsheet.AddWorkbookPart();
+            workbookPart.Workbook = new Workbook();
+
+            var worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+            worksheetPart.Worksheet = new Worksheet(new SheetData());
+
+            Sheets sheets = spreadsheet.WorkbookPart.Workbook.AppendChild(new Sheets());
+            Sheet sheet = new Sheet() { Id = spreadsheet.WorkbookPart.GetIdOfPart(worksheetPart), SheetId = 1, Name = "Timeline" };
+            sheets.Append(sheet);
+
+            SheetData sheetData = worksheetPart.Worksheet.GetFirstChild<SheetData>();
+
+            // Header
+            Row headerRow = new Row();
+            headerRow.Append(
+                CreateCell("Room"),
+                CreateCell("Devotee"),
+                CreateCell("Code"),
+                CreateCell("Allocated"),
+                CreateCell("From"),
+                CreateCell("To")
+            );
+            sheetData.Append(headerRow);
+
+            foreach (var r in list)
+            {
+                Row row = new Row();
+                row.Append(
+                    CreateCell(r.RoomName),
+                    CreateCell(r.DevoteeName),
+                    CreateCell(r.DevoteeCode),
+                    CreateCell(r.Allocated.ToString()),
+                    CreateCell(r.FromDate.ToString("dd-MM-yyyy")),
+                    CreateCell(r.ToDate.ToString("dd-MM-yyyy"))
+                );
+                sheetData.Append(row);
+            }
+        }
+
+        return File(stream.ToArray(),
+                    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    "ReservationTimeline.xlsx");
+    }
+
+    private Cell CreateCell(string text) =>
+        new Cell(new CellValue(text)) { DataType = CellValues.String };
 
 }
 
